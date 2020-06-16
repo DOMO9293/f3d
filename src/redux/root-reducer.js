5f81276b0a52b961fcb37b0814921f0e290d4d6b
DOMO9293 <flqjsl@gmail.com>
2020-06-17 00:02:45 +0200

    redux-persist added

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/user.reducer";
+import scrapReducer from "./scrap/scrap.reducer";
 
-export default combineReducers({
+const rootReducer = combineReducers({
   user: userReducer,
+  scrap: scrapReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["scrap"],
+};
+
+export default persistReducer(persistConfig, rootReducer);
